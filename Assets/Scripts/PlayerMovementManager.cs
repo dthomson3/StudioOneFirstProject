d99329f6c3b28394218b7ad23c189280d02dff85
SAE\dthomson3 <dylan.levi.thomson@gmail.com>
2018-02-14 07:28:20 +0800

    restricted player movement to bounding box

--- Assets/Scripts/PlayerMovementManager.cs
+++ Assets/Scripts/PlayerMovementManager.cs
@@ -7,28 +7,36 @@ public class PlayerMovementManager : MonoBehaviour {
     public float playerMoveSpeed;
     public float playerJumpSpeed;
     public float fallMultiplier;
+    public bool hasJumped;
     Rigidbody2D rb;
     public Bounds AABBsize;
+    Vector3 offset;
 
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
-        
+        offset = transform.parent.position - transform.position;
     }
-	
-	// Update is called once per frame
-	void Update () {
-        
+
+    // Update is called once per frame
+    void Update () {
+        AABBsize.center = transform.parent.position - offset ;
 
         Move();
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetAxis("Vertical") > 0)
+        {
             Jump();
+        }
         JumpPartTwo();
 	}
 
     void Jump()
     {
-        rb.velocity = Vector2.up * playerJumpSpeed;
+        if (hasJumped != true)
+        {
+            rb.velocity = Vector2.up * playerJumpSpeed;
+            hasJumped = true;
+        }
     }
 
     void JumpPartTwo()
@@ -43,10 +51,23 @@ public class PlayerMovementManager : MonoBehaviour {
     {
         float inputX = Input.GetAxis("Horizontal");
         transform.position += Vector3.right * inputX * Time.deltaTime * playerMoveSpeed;
+        if (!AABBsize.Contains(transform.position))
+        {
+        transform.position -= Vector3.right * inputX * Time.deltaTime * playerMoveSpeed;
+
+        }
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.DrawCube(AABBsize.center, AABBsize.size);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Floor"))
+        {
+            hasJumped = false;
+        }
     }
 }
